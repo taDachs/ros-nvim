0b2e81f87eddb25ae9adefdddca8797deb7f9ed0
Max Schik <max.schik@googlemail.com>
2023-02-10 21:38:30 +0100

    adds check for correct number of args for rosed

--- lua/ros-nvim/init.lua
+++ lua/ros-nvim/init.lua
@@ -7,6 +7,10 @@ function M.setup(opts)
 end
 
 vim.api.nvim_create_user_command("Rosed", function(opts)
+                                            if #opts.fargs ~= 2 then
+                                              vim.notify("Error, Rosed need exactly two arguments")
+                                              return
+                                            end
                                             ros.rosed(opts.fargs[1], opts.fargs[2])
                                           end,
                                           {
