254e6965c290e26fd940f58aa031e667f0a6a9e5
Max Schik <max.schik@googlemail.com>
2023-03-02 01:32:36 +0100

    fix: removes unnecessary import for mutils and adds missing function

--- lua/ros-nvim/telescope/pickers.lua
+++ lua/ros-nvim/telescope/pickers.lua
@@ -4,16 +4,27 @@ local conf = require("telescope.config").values
 
 local ros = require("ros-nvim.ros")
 local ros_config = require("ros-nvim.config")
-local util = require("mutils")
 
 local ros_pickers = {}
+
+-- both must me absolute
+local function relative_path(path, parent)
+  local path_split = vim.fn.split(path, "/")
+  local parent_split = vim.fn.split(parent, "/")
+  local len_parent = 0
+  for i, _ in ipairs(parent_split) do
+    len_parent = i
+  end
+  local relative_split = { unpack(path_split, len_parent + 1) }
+  return table.concat(relative_split, "/")
+end
 
 local function build_result_list()
   local results = {}
   for _, pkg_name in pairs(ros.handle:list_ws_scope_pkg_names(ros_config.telescope.ws_filter)) do
     local pkg = ros.handle:get_pkg(pkg_name)
     for _, file in pairs(pkg.files) do
-      local val = pkg.name .. " " .. util.relative_path(file, pkg.path)
+      local val = pkg.name .. " " .. relative_path(file, pkg.path)
       table.insert(results, {
         display = val,
         value = file,
